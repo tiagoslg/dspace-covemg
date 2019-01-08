201d18611ad2026c17b8af8a328ecc38e7a94a1f
tiagoslg <tiagoslg@gmail.com>
2019-01-07 23:00:12 -0200

    correcoes na geracao de arquivo

--- rest_lib.py
+++ rest_lib.py
@@ -2,9 +2,37 @@ import requests
 import os
 from db_lib import get_internal_id, get_file_type_size
 import csv
+import shutil
 
 
-BASE_DIR = '/home/tiagoslg/projects/dspace-covemg/'
+BASE_DIR = '/home/tiagoslg/projects/dspace-covemg/DSpace_COVEMG/'
+
+
+def remove_caracter_especial(texto):
+    d = {'À': 'A', 'Á': 'A', 'Ä': 'A', 'Â': 'A', 'Ã': 'A',
+         'È': 'E', 'É': 'E', 'Ë': 'E', 'Ê': 'E',
+         'Ì': 'I', 'Í': 'I', 'Ï': 'I', 'Î': 'I',
+         'Ò': 'O', 'Ó': 'O', 'Ö': 'O', 'Ô': 'O', 'Õ': 'O',
+         'Ù': 'U', 'Ú': 'U', 'Ü': 'U', 'Û': 'U',
+         'à': 'a', 'á': 'a', 'ä': 'a', 'â': 'a', 'ã': 'a', 'ª': 'a',
+         'è': 'e', 'é': 'e', 'ë': 'e', 'ê': 'e',
+         'ì': 'i', 'í': 'i', 'ï': 'i', 'î': 'i',
+         'ò': 'o', 'ó': 'o', 'ö': 'o', 'ô': 'o', 'õ': 'o', 'º': 'o', '°': 'o',
+         'ù': 'u', 'ú': 'u', 'ü': 'u', 'û': 'u',
+         'Ç': 'C', 'ç': 'c',
+         ':': '_', '(': '', ')': ''}
+    novo_texto = ''
+
+    for c in range(0, len(texto)):
+        novo_caracter = texto[c]
+        try:
+            novo_caracter = d[texto[c]]
+        except KeyError:
+            pass
+
+        novo_texto = novo_texto + novo_caracter
+
+    return novo_texto
 
 
 def get_session_cookie():
@@ -109,27 +137,29 @@ def create_file_list():
     for com in hie_json['community']:
         com_path = []
         com_path.extend(base_path_list)
-        com_path.append(com['name'])
+        com_path.append(com['name'].replace('/', ''))
         for sub_com in com['community']:
             sub_com_path = []
             sub_com_path.extend(com_path)
-            sub_com_path.append(sub_com['name'])
+            sub_com_path.append(sub_com['name'].replace('/', ''))
             for col in sub_com['collection']:
                 col_path = []
                 col_path.extend(sub_com_path)
-                col_path.append(col['name'])
+                col_path.append(col['name'].replace('/', ''))
                 items = get_collection_items(col['id']).json()
                 for item in items:
                     item_path = []
                     item_path.extend(col_path)
-                    item_path.append(item['name'])
+                    item_path.append(item['name'].replace('/', ''))
                     bitstreams = get_item_bitstreams(item['uuid']).json()
                     for bitstream in bitstreams:
                         internal_id = get_internal_id(bitstream['uuid'])[0]
                         type_size = get_file_type_size(internal_id)
+                        path = '/'.join(item_path).replace('//', '/')
+                        path = remove_caracter_especial('_'.join(path.split()))
                         bitstream_dict = {
-                            'path': '/'.join(item_path).replace('//', '/'),
-                            'name': bitstream['name'],
+                            'path': path,
+                            'name': remove_caracter_especial('_'.join(bitstream['name'].split())),
                             'internal_id': internal_id,
                             'relative_path': '{}/{}/{}'.format(internal_id[:2],
                                                                internal_id[2:4],
@@ -142,19 +172,21 @@ def create_file_list():
         for col in com['collection']:
             col_path = []
             col_path.extend(com_path)
-            col_path.append(col['name'])
+            col_path.append(col['name'].replace('/', ''))
             items = get_collection_items(col['id']).json()
             for item in items:
                 item_path = []
                 item_path.extend(col_path)
-                item_path.append(item['name'])
+                item_path.append(item['name'].replace('/', ''))
                 bitstreams = get_item_bitstreams(item['uuid']).json()
                 for bitstream in bitstreams:
                     internal_id = get_internal_id(bitstream['uuid'])[0]
                     type_size = get_file_type_size(internal_id)
+                    path = '/'.join(item_path).replace('//', '/')
+                    path = remove_caracter_especial('_'.join(path.split()))
                     bitstream_dict = {
-                        'path': '/'.join(item_path).replace('//', '/'),
-                        'name': bitstream['name'],
+                        'path': path,
+                        'name': remove_caracter_especial('_'.join(bitstream['name'].split())),
                         'internal_id': internal_id,
                         'relative_path': '{}/{}/{}'.format(internal_id[:2],
                                                            internal_id[2:4],
@@ -172,24 +204,26 @@ def create_file_list():
     com_path.append(com['name'])
     sub_com_path = []
     sub_com_path.extend(com_path)
-    sub_com_path.append(sub_com['name'])
-    sub_com_path.append(sub_com_2['name'])
+    sub_com_path.append(sub_com['name'].replace('/', ''))
+    sub_com_path.append(sub_com_2['name'].replace('/', ''))
     for col in sub_com_2['collection']:
         col_path = []
         col_path.extend(sub_com_path)
-        col_path.append(col['name'])
+        col_path.append(col['name'].replace('/', ''))
         items = get_collection_items(col['id']).json()
         for item in items:
             item_path = []
             item_path.extend(col_path)
-            item_path.append(item['name'])
+            item_path.append(item['name'].replace('/', ''))
             bitstreams = get_item_bitstreams(item['uuid']).json()
             for bitstream in bitstreams:
                 internal_id = get_internal_id(bitstream['uuid'])[0]
                 type_size = get_file_type_size(internal_id)
+                path = '/'.join(item_path).replace('//', '/')
+                path = remove_caracter_especial('_'.join(path.split()))
                 bitstream_dict = {
-                    'path': '/'.join(item_path).replace('//', '/'),
-                    'name': bitstream['name'],
+                    'path': path,
+                    'name': remove_caracter_especial('_'.join(bitstream['name'].split())),
                     'internal_id': internal_id,
                     'relative_path': '{}/{}/{}'.format(internal_id[:2],
                                                        internal_id[2:4],
@@ -201,10 +235,20 @@ def create_file_list():
                 print(file_list.__len__())
     return file_list
 
-# with open('file_list.csv') as csv_file:
-#     reader = csv.DictReader(csv_file)
-#     for row in reader:
-#         os.makedirs(row['path'], exist_ok=True)
-#         shutil.copy2('/home/tiagoslg/Documentos/assetstore/{}/{}'.format(row['relative_path'],
-#                                                                          row['internal_id']),
-#                      '{}/{}'.format(row['path'], row['name']))
+
+def list_to_csv(list):
+    keys = list[0].keys()
+    with open('file_list.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(list)
+
+
+def reorder_files():
+    with open('file_list.csv') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            os.makedirs(row['path'], exist_ok=True)
+            shutil.copy2('/home/tiagoslg/Documentos/assetstore/{}/{}'.format(row['relative_path'],
+                                                                             row['internal_id']),
+                         '{}/{}'.format(row['path'], row['name']))
